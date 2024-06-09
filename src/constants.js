a14391403898f61daab7343821bde4eb7d2d8ddf
Sam <frostrik@gmail.com>
2024-06-09 17:23:40 -0600

    Project Created and finished

---
+++ src/constants.js
@@ -0,0 +1,23 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript/Java these days.
+    I've made a  some number of projects in that lenguage. I also like Salesforce and Python. This is my Github Portfile if you want to know more
+    <a href="https://github.com/sam-alen" target="_blank">Github</a>!`,
+  "cs-degree": `This is my Certification of Generation Bootcamp, i gust get the knowledge of Java, JavaScript, and other tools, very helpful and it was a really challenge for the time that spent in this Bootcamp`,
+  "cs-react":  `This is my React Certificate, i completed the path of Fronted Developer at the Coursera course by Meta`,
+  "cs-ds":  `I took a bootcamp course of DS in my free time, this is by Tripleten Platform.`,
+  "cs-administrator":  `This is my Salesforce Administrator certificaction, i studied so hard to get it and i'm really proud of it.`,
+  "cs-b2c":  `My First certificaction of Salesforce, Salesforce B2C Developer, i worked with Salesforce for a while and i got this certificate`,
+  "memorial": `Memorial for the fallen of the CH41, the people that jus tried to reach the goal and just fall in the middle of the path.`,
+  "sofa-table": `Thats my sofa, i really enjoy to be here and relax when i see some tv series in my tablet or using apps like Duolingo or Brilliant.`,
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like Midudev and FreeCodeCamp`,
+  bed: `This is my bed, i really enjoy to sleep and read some books in my bed before sleep`,
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/JSLegendDev/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
+  Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : I make this portfolio following the tutorial of freecodecamp, the tutorial was created by 
+  JSLegend, if you want to see more of his work <a href="https://youtube.com/@jslegenddev" target="_blank">Click here</a>.`,
+  library: `There are a lot of programming books on my shelves. Now i'm reading how to crack the code interview, it helps me to understand some technical problems, but my favorite author
+  is Ryan Holiday`,
+  exit: `If you want to exit of this  portfolio, just close the tab.`,
+};
